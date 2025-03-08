ae3737ff619e65673ac382b0ba718f0468776037
Yuna <kn9012@naver.com>
2025-03-08 14:05:00 +0900

    Update LeetCode_875_KokoEatingBananas.java

--- 250310/LeetCode_875_KokoEatingBananas.java
+++ 250310/LeetCode_875_KokoEatingBananas.java
@@ -1,3 +1,5 @@
+// 첫번째 풀이 : 29ms
+
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
@@ -10,13 +12,39 @@ class Solution {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            int count = 0;
+            int hour = 0;
             for (int pile : piles) {
-                count += Math.ceil((double) pile / mid);
+                hour += Math.ceil((double) pile / mid);
             }
 
-            if (count <= h) {
+            if (hour <= h) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
+
+// 두번째 풀이 : 7ms
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 0;
+        for (int pile : piles) right = Math.max(right, pile);
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            int hour = 0;
+            for (int pile : piles) {
+                hour += (pile + mid - 1) / mid;
+            }
+
+            if (hour <= h) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
